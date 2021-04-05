20dbfa037fec2b4696cb82443d12354aa92f1041
plerros <48536508+plerros@users.noreply.github.com>
2021-04-05 09:06:29 +0300

    Include gplv3 header
    From now on as long as you follow the license, you don't have to ask my permission to use this code

--- exam-template-file.s
+++ exam-template-file.s
@@ -1,4 +1,19 @@
+QTSPIM MIPS assembly template
+Copyright (C) 2020,2021  Pierro Zachareas
 
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ 
 #___________________________________CODE___________________________________
 .text
 .globl __start
